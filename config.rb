7d3eea12dfd24dbeea5eaaf96d117e85c609f720
Mila Frerichs <mila.frerichs@gmail.com>
2015-04-22 11:53:31 +0200

    Update config file
    enable live-reload
    add sprockets and bower files

--- config.rb
+++ config.rb
@@ -1,12 +1,26 @@
-###
-# Compass
-###
 
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
+configure :development do
+  activate :livereload
+end
 
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+
+activate :sprockets
+sprockets.append_path File.join "#{root}", "bower_components"
+
+configure :build do
+end
+activate :deploy do |deploy|
+  deploy.build_before = true # default: false
+  deploy.method = :git
+end
+activate :relative_assets
+set :relative_links, true
+#
 ###
 # Page options, layouts, aliases and proxies
 ###
@@ -36,9 +50,6 @@
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -46,33 +57,3 @@
 #     "Helping"
 #   end
 # end
-
-set :css_dir, 'stylesheets'
-
-set :js_dir, 'javascripts'
-
-set :images_dir, 'images'
-
-# Build-specific configuration
-configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
-end
-activate :deploy do |deploy|
-  deploy.build_before = true # default: false
-  deploy.method = :git
-end
-activate :relative_assets
-set :relative_links, true
